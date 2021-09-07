693d469c831ac96d1b1e53f9d3b0449b70ec80a9
Rob Koster <robkoster2907@gmail.com>
2021-09-07 09:23:31 +0200

    Ik heb een python programma gemaakt die om je NAW vraagt

---
+++ person.py
@@ -0,0 +1,14 @@
+from os import name
+
+Naam = input("Enter Naam:")
+Adres = input("Enter Adres:")
+Postcode = input("Enter Postcode:")
+Woonplaats = input("Enter Woonplaats:")
+
+
+print("----------------------------------------------")
+print("|  Naam: " + Naam) 
+print("|  Adres: " + Adres) 
+print("|  Postcode: " + Postcode) 
+print("|  Woonplaats: " + Woonplaats) 
+print("----------------------------------------------")
